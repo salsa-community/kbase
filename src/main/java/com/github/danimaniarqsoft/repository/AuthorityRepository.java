2b32dd1ed0b7411f88ee14be63533ea076560cb1
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2019-11-22 23:00:02 -0600

    first commit

---
+++ src/main/java/com/github/danimaniarqsoft/repository/AuthorityRepository.java
@@ -0,0 +1,10 @@
+package com.github.danimaniarqsoft.repository;
+
+import com.github.danimaniarqsoft.domain.Authority;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {
+}
