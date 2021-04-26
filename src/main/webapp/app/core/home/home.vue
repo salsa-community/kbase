7b4473ab76f76053c6077aa777617b5effd3c0d0
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2021-04-26 15:42:37 -0500

    update home

--- src/main/webapp/app/core/home/home.vue
+++ src/main/webapp/app/core/home/home.vue
@@ -3,10 +3,10 @@
     <div class="row">
       <div class="col-md">
         <div v-if="authenticated">
-          <img src="content/images/knowledgebase.gif" alt="ciencia-loader" width="100%" />
+          <img src="content/images/knowledgebase.gif" alt="ciencia-loader" @load="onImgLoad" width="100%" />
         </div>
         <div v-if="!authenticated" class="text-center">
-          <img src="content/images/kbase-logo.png" alt="ciencia-loader" @load="onImgLoad" style="max-width: 700px; width: 100%" />
+          <img src="content/images/kbase-logo.png" alt="ciencia-loader" @load="onImgLoad" style="max-width: 800px; width: 100%" />
         </div>
         <div v-if="!authenticated && imgLoaded" class="col-md text-center mt-4">
           <b-button size="lg" v-on:click="openLogin()" variant="outline-primary">Iniciar sesión</b-button>
