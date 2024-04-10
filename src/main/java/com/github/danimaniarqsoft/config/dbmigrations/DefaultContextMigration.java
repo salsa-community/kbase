6293b09ad214552bbf654d8732140e56893973ef
daniel <daniel.cortes.pichardo@gmail.com>
2024-04-09 18:56:51 -0600

    add: default context with flow

--- src/main/java/com/github/danimaniarqsoft/config/dbmigrations/DefaultContextMigration.java
+++ src/main/java/com/github/danimaniarqsoft/config/dbmigrations/DefaultContextMigration.java
@@ -1,9 +1,12 @@
 package com.github.danimaniarqsoft.config.dbmigrations;
 
 import com.github.danimaniarqsoft.domain.Contexto;
-
+import com.github.danimaniarqsoft.domain.Mensaje;
 import com.github.mongobee.changeset.ChangeLog;
 import com.github.mongobee.changeset.ChangeSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
 
@@ -22,6 +25,23 @@ public class DefaultContextMigration {
         contexto.setDescEn("Default context");
         contexto.setNombre("default context");
         contexto.setOrganizacion("Kbase");
+        contexto.setMensajes(defaultMessageList());
         mongoTemplate.save(contexto);
+    }
+
+    private static List<Mensaje> defaultMessageList(){
+        List<Mensaje> messages = new ArrayList<>();
+        messages.add(createMessage("Bienvenido a $ORGANIZACION","Welcome to $ORGANIZACION", 1));
+        messages.add(createMessage("Este es un flujo demo para el contexto por default","This is a demo flow", 2));
+        messages.add(createMessage("Configura estos flujos desde la consola del kbase","Config this dialog into the kbase", 3));
+        return messages;
+    }
+
+    private static Mensaje createMessage(String desc, String descEn, Integer order){
+        Mensaje message = new Mensaje();
+        message.setDesc(desc);
+        message.setDescEn(descEn);
+        message.setOrden(order);
+        return message;
     }
 }
